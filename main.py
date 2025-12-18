d9de9681667cf3b68bf5cbeed2d880990a278f83
Brian Bianchi <bianchibrian2@gmail.com>
2025-12-18 17:53:21 -0500

    main script

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+#2. Knead the dough for 10 minutes.
+#3. Add 3g of Salt.
+#4. Leave to rise for 2 hours.
+#5. Bake at 200 degrees C for 30 minutes.
+
+def print_phrase(string):
+    print(f'{string}')
+
+
+if __name__ == '__main__':
+    print_phrase("\n" + input("Watchu want\n") + "\n")
